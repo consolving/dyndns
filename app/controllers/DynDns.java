99bcaa16a88a3fe7ce4d8d1b5600944b7409cc14
Philipp Haussleiter <philipp@haussleiter.de>
2017-01-22 17:06:46 +0100

    fixing compile errors

--- app/controllers/DynDns.java
+++ app/controllers/DynDns.java
@@ -11,7 +11,7 @@ import models.SubDomain;
 import play.data.Form;
 import play.mvc.Result;
 import views.html.DynDns.index;
-import views.html.DynDns.hosts;
+import views.txt.DynDns.hosts;
 import fileauth.actions.BasicAuth;
 
 @BasicAuth
